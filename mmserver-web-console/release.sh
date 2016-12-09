a3d08a09d30f240b15c93c8653d9cc427046a15d
maomao <1498653869@qq.com>
2016-12-09 16:43:12 +0800

    Commit

--- mmserver-web-console/release.sh
+++ mmserver-web-console/release.sh
@@ -6,7 +6,20 @@
 #  Author :huxg
 #--------------------------------------------
 
-serverDir=/home/huxg/Workspace/Git/Git_Ace/hdp/node_deploy
+this_dir=`pwd`
+dirname $0|grep "^/" >/dev/null
+if [ $? -eq 0 ];then
+    this_dir=`dirname $0`
+else
+    dirname $0|grep "^\." >/dev/null
+    retval=$?
+    if [ $retval -eq 0 ];then
+        this_dir=`dirname $0|sed "s#^.#$this_dir#"`
+    else
+        this_dir=`dirname $0|sed "s#^#$this_dir/#"`
+    fi
+fi
 
+SERVER_DIR="${this_dir%/*}/node_deploy"
 
-fis3 release development -wcd "$serverDir"
+fis3 release development -wcd "$SERVER_DIR"
